96f37bf95c071fe26dc3feaa5ed88a57b6a9ea84
Daniel Ryan <31455292+DNRyan@users.noreply.github.com>
2024-02-26 15:44:03 +0000

    move summary into forecast day

--- src/forecast.py
+++ src/forecast.py
@@ -15,18 +15,11 @@ class ForecastDay:
     day: date
     data: list[ForecastData]
 
-
-def summarize_forecast(data):
-    summaries = {}
-
-    grp_day = group_by_day(data)
-
-    # Process each day
-    for forecast_day in grp_day:
+    def summary(self) -> dict[str, object]:
         morning_t, morning_r, afternoon_t, afternoon_r = [], [], [], []
-        all_t = [entry.average_temperature for entry in forecast_day.data]
+        all_t = [entry.average_temperature for entry in self.data]
 
-        for e in forecast_day.data:
+        for e in self.data:
             # collect morning period entries
             if 6 <= e.date_time.hour < 12:
                 morning_t.append(e.average_temperature)
@@ -36,25 +29,26 @@ def summarize_forecast(data):
                 afternoon_t.append(e.average_temperature)
                 afternoon_r.append(e.probability_of_rain)
 
-        summary = {
+        return {
             "morning_average_temperature": rounded_average(morning_t),
             "morning_chance_of_rain": rounded_average(morning_r, 2),
             "afternoon_average_temperature": rounded_average(afternoon_t),
             "afternoon_chance_of_rain": rounded_average(afternoon_r, 2),
             "high_temperature": max(all_t),
-            "low_temperature": min(all_t)
+            "low_temperature": min(all_t),
         }
 
-        # format reader-friendly date
-        day_name = format_date(forecast_day.day)
 
-        summaries[day_name] = summary
-
+def summarize_forecast(data):
+    grp_day = group_by_day(data)
+    summaries = {format_date(fd.day): fd.summary() for fd in grp_day}
     return summaries
 
 
 def rounded_average(data, round_to=0):
-    return round(sum(data) / len(data), round_to) if data else "Insufficient forecast data"
+    return (
+        round(sum(data) / len(data), round_to) if data else "Insufficient forecast data"
+    )
 
 
 def format_date(day: datetime):
@@ -64,13 +58,13 @@ def format_date(day: datetime):
 def group_by_day(data) -> list[ForecastDay]:
     grp_day: dict[date, list[ForecastData]] = defaultdict(list)
     for e in data:
-        entry_time = datetime.fromisoformat(e["date_time"].replace('Z', '+00:00'))
+        entry_time = datetime.fromisoformat(e["date_time"].replace("Z", "+00:00"))
         key = entry_time.date()
         grp_day[key].append(
             ForecastData(
                 date_time=entry_time,
                 average_temperature=e["average_temperature"],
-                probability_of_rain=e["probability_of_rain"]
+                probability_of_rain=e["probability_of_rain"],
             )
         )
     return [ForecastDay(day=k, data=v) for k, v in grp_day.items()]
